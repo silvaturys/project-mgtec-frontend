cfaec431fd6eabfeb39d7ea7ef62fea6b805cb98
silvaturys <fernandes.arthur2704@gmail.com>
2025-04-07 00:05:16 -0300

    responsive part of home and quem somos

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,8 +8,8 @@ function Home() {
     <>
     <section className="home" id="home">
       <h1 className="home__title">A tecnologia que sua casa precisa !</h1>
-      <p className="home__paragraph_1">Soluções completas em tecnologia para sua casa do futuro.</p>
-      <p className="home__paragraph_2" >Criamos ambientes inteligentes, conectados e personalizados para você.</p>
+      <p className="home__paragraph">Soluções completas em tecnologia para sua casa do futuro.</p>
+      <p className="home__paragraph" >Criamos ambientes inteligentes, conectados e personalizados para você.</p>
       <a href="#contato" className="home__botton">Conheça a MGTEC</a>
     </section>
     <QuemSomos></QuemSomos>
